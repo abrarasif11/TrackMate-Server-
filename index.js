78bbcb5bb0148e02640284403996fefeed18a2b5
abrarasif11 <faasif929@gmail.com>
2025-09-17 23:35:06 +0600

    create payment intern

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
+
+// Load environment variables
 require("dotenv").config();
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb"); //
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+const stripe = require("stripe")(process.env.PAYMENT_GATEWAY_KEY);
 
 // Middleware
+
 app.use(cors());
 app.use(express.json());
 
@@ -58,6 +64,40 @@ async function run() {
       }
     });
 
+    //  GET single parcel by MongoDB _id
+    app.get("/parcels/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+
+        // Validate ObjectId
+        if (!ObjectId.isValid(id)) {
+          return res.status(400).json({
+            success: false,
+            message: "Invalid parcel ID",
+          });
+        }
+
+        // Find parcel by _id
+        const parcel = await parcelCollection.findOne({
+          _id: new ObjectId(id),
+        });
+
+        if (!parcel) {
+          return res.status(404).json({
+            success: false,
+            message: "Parcel not found",
+          });
+        }
+
+        res.json({
+          success: true,
+          data: parcel,
+        });
+      } catch (error) {
+        res.status(500).json({ success: false, error: error.message });
+      }
+    });
+
     //  POST API -> Add new parcel
     app.post("/parcels", async (req, res) => {
       try {
@@ -102,6 +142,24 @@ async function run() {
         res
           .status(500)
           .json({ message: "Internal server error", error: error.message });
+      }
+    });
+
+    // Create Payment Intent endpoint
+    app.post("/create-payment-intent", async (req, res) => {
+      const amountInCents = req.body.amountInCents;
+      try {
+        const paymentIntent = await stripe.paymentIntents.create({
+          amount: amountInCents,
+          currency: "usd",
+          payment_method_types: ["card"],
+        });
+
+        res.json({
+          clientSecret: paymentIntent.client_secret,
+        });
+      } catch (error) {
+        res.status(500).json({ error: error.message });
       }
     });
 
